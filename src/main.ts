0ee6852e7aa84768d043822edf8f4c6f8942904e
Aziz Zina <aziz.zina2001@gmail.com>
2026-01-16 08:36:08 +0100

    add vercel speed insights

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { appConfig } from './app/app.config';
 import { inject } from "@vercel/analytics"
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
 inject();
+injectSpeedInsights();
